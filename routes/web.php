8a10f8e6dd3e9c35be7766f924231c6acfefcacd
valentinafu <valentinafurxhi@gmail.com>
2017-12-30 07:37:36 -0800

    Some dummy useless commit

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    $notifications = DB::table('notifications')->get();
+    return view('home',compact('notifications'));
 })->name('home');
 
 Route::get('/test', function () {
@@ -27,3 +28,6 @@ Route::get('/test', function () {
 Route::get('login', 'Auth\AuthController@redirectToProvider');
 Route::get('login/callback', 'Auth\AuthController@handleProviderCallback');
 Route::post('logout', 'Auth\AuthController@logout');
+Route::post('exams/{exam}/post', 'ExamController@postExam')->name('exams');
+Route::get('exams/{exam}/edit', 'ExamController@editExam');
+Route::get('exams/{exam}/delete', 'ExamController@deleteExam');
